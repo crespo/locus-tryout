2e769520cec88dd40380afa5c973e8f72b2647f9
raul crespo <raulcrespo@outlook.com.br>
2022-02-17 15:49:03 -0300

    added not nullable column

--- src/main/java/software/locus/tryout/Model/Cidade.java
+++ src/main/java/software/locus/tryout/Model/Cidade.java
@@ -18,17 +18,17 @@ public class Cidade {
     @Column(name = "cidade_id")
     private int id;
 
-    @Column(name = "nome")
+    @Column(name = "nome", nullable = false)
     private String nome;
 
-    @Column(name = "populacao")
+    @Column(name = "populacao", nullable = false)
     private int populacao;
 
     @ManyToOne(targetEntity = Estado.class, fetch = FetchType.EAGER)
     @JoinColumn(name = "estado_id", insertable = false, updatable = false)
     private Estado estado;
 
-    @Column(name = "estado_id")
+    @Column(name = "estado_id", nullable = false)
     private int estadoId;
 
 }
