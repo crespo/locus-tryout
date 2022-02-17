e8f686f41e1b46eb6962b737789dfb8877b4b3a1
raul crespo <raulcrespo@outlook.com.br>
2022-02-17 16:13:11 -0300

    added put and delete methods

--- src/main/java/software/locus/tryout/Controller/EstadoController.java
+++ src/main/java/software/locus/tryout/Controller/EstadoController.java
@@ -1,7 +1,9 @@
 package software.locus.tryout.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import software.locus.tryout.Model.Estado;
 import software.locus.tryout.Service.EstadoService;
 
@@ -15,11 +17,34 @@ public class EstadoController {
 
     @PostMapping("/addEstado")
     public Estado addEstado(@RequestBody Estado estado) {
-        return estadoService.saveEstado(estado);
+        if (estadoService.saveEstado(estado)) {
+            throw new ResponseStatusException(HttpStatus.OK, "State created.");
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Something's wrong.");
+        }
     }
 
     @GetMapping("/estados")
     public List<Estado> findAllEstados() {
         return estadoService.getEstados();
     }
+
+    @PutMapping("/updateEstado")
+    public Estado updateEstado(@RequestBody Estado estado) {
+        if (estadoService.updateEstado(estado)) {
+            throw new ResponseStatusException(HttpStatus.OK, "State updated.");
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Something's wrong.");
+        }
+    }
+
+    @DeleteMapping("/deleteEstado/{id}")
+    public String deleteEstado(@PathVariable int id) {
+        if (estadoService.deleteEstado(id)) {
+            throw new ResponseStatusException(HttpStatus.OK, "State updated.");
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Something's wrong.");
+        }
+    }
+
 }
--- src/main/java/software/locus/tryout/Service/EstadoService.java
+++ src/main/java/software/locus/tryout/Service/EstadoService.java
@@ -13,12 +13,39 @@ public class EstadoService {
     @Autowired
     private EstadoRepository estadoRepository;
 
-    public Estado saveEstado(Estado estado) {
-        return estadoRepository.save(estado);
+    public boolean saveEstado(Estado estado) {
+        if (estadoRepository.existsById(estado.getId())) {
+            estadoRepository.save(estado);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public List<Estado> getEstados() {
         return estadoRepository.findAll();
     }
 
+    public boolean updateEstado(Estado estado) {
+        Estado existingEstado = estadoRepository
+                                    .findById(estado.getId())
+                                    .orElse(null);
+
+        if (existingEstado != null) {
+            existingEstado.setNome(estado.getNome());
+            estadoRepository.save(existingEstado);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean deleteEstado(int id) {
+        if (estadoRepository.existsById(id)) {
+            estadoRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
